c99c7ecac683ecf689aad867f5f3d691a7228f40
savi2009 <savita.ak2009@gmail.com>
2022-07-08 20:04:29 +0530

    Multiple files added

--- add.java
+++ add.java
@@ -1,7 +1,8 @@
 This is add function for application
-Removed by me
+Replaced with original
 This line is added by megha
 Please add new function for this feature
  This new file need to create in devops
  and thios could be good
  this is gani
+This file is operated by all of us
