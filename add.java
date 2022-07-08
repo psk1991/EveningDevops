7a0abd44b84d13e8bd95ec4e91c10bbf68c5e6ac
savi2009 <savita.ak2009@gmail.com>
2022-07-08 20:27:35 +0530

    Final commit for conflict

--- add.java
+++ add.java
@@ -1,8 +1,9 @@
 This is add function for application
 Replaced with original
-This line is added by megha
 Please add new function for this feature
  This new file need to create in devops
  and thios could be good
  this is gani
 This file is operated by all of us
+Megha's line is deketed by me ; was 3rd line
+
